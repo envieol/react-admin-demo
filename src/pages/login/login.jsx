073cc2bb6320344e2b506b8bd16dd23cc18b5c95
envieol <371755714@qq.com>
2020-05-23 18:37:47 +0800

    test

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,6 +5,31 @@ import "./login.less"
 import logo from "./imgs/logo.svg"
 
 const Login = () => {
+
+    const [form] = Form.useForm();
+
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+
+
+    const handleFrom = async ()=>{
+        try {
+            const values = await form.validateFields();
+            //TODO send valuse to back end
+            console.log('Success:', values);
+        } catch (errorInfo) {
+            console.log('Failed:', errorInfo);
+        }
+    }
+
+    const pwValidator =  (rule,value)=>{
+        if (!value || form.getFieldValue('password') === 'pw') {
+            return Promise.resolve();
+        }
+        return Promise.reject('password must be "pw"');
+    }
+
     return (
         <div className={"login"}>
             <header className={"login-header"}>
@@ -13,16 +38,29 @@ const Login = () => {
             </header>
             <section className={"login-content"}>
                 <h2>Sign In</h2>
-                <Form className={"login-form"}>
-                    <Form.Item>
+                <Form form={form}
+                      className={"login-form"}
+                      name={"loginForm"}
+                      onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
                         <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
                     </Form.Item>
-                    <Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' ,validator:pwValidator}]}
+                    >
                         <Input prefix={<LockOutlined className="site-form-item-icon"/>} type="password"
                                placeholder="Password"/>
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit" className={"login-form-button"}> Login </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type={"ghost"} className={"login-form-handle"} onClick={handleFrom}>handle</Button>
                     </Form.Item>
                 </Form>
             </section>
@@ -30,4 +68,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default  Login
